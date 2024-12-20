ce4a6381c4f4f46fe4973430ed31fb1695d61891
Lagerwerf <r.m.lagerwerf@student.tue.nl>
2024-12-20 18:24:09 +0100

    feature: display top contributors accross 20 last updated repositories

--- index.php
+++ index.php
@@ -27,117 +27,114 @@ function loadEnv($filePath = '.env')
     }
 }
 
+function sendRequest($ch, $url)
+{
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $response = curl_exec($ch);
+    return json_decode($response, true);
+}
+
 // Load the .env file
 loadEnv();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Replace with your GitHub organization and repository
-$organization = "GEWIS";
-$repo = "sudosos-frontend";
-$accessToken = getenv('ACCESS_TOKEN');
-// GitHub API URL to fetch pull requests
-$url = "https://api.github.com/orgs/GEWIS/repos?per_page=100";
-// Use cURL to fetch data
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     "Accept: application/vnd.github+json",
-    "Authorization: Bearer $accessToken",
+    "Authorization: Bearer " . getenv('ACCESS_TOKEN'),
     "X-GitHub-Api-Version: 2022-11-28",
     "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0"
 ]);
 
-$response = curl_exec($ch);
-$repos = json_decode($response, true);
-$activities = array();
-foreach ($repos as $repo) {
-    echo $repo['name'] . "<br>";
-    $name = $repo['name'];
-    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/GEWIS/$name/activity");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Accept: application/vnd.github+json",
-        "Authorization: Bearer $accessToken",
-        "X-GitHub-Api-Version: 2022-11-28",
-        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0"
-    ]);
-    $activity = json_decode(curl_exec($ch), true);
-    foreach ($activity as $activityItem) {
-        $simplifiedActivity = array();
-        $simplifiedActivity['id'] = $activityItem['id'];
-        $simplifiedActivity['repo'] = $repo['name'];
-        $simplifiedActivity['timestamp'] = $activityItem['timestamp'];
-        $simplifiedActivity['actor'] = $activityItem['actor']['login'];
-        $simplifiedActivity['gravatar'] = $activityItem['actor']['avatar_url'];
-        $activities[] = $simplifiedActivity;
-    }
-}
-// Sort activities by timestamp (newest to oldest)
-usort($activities, function ($a, $b) {
-    // Compare the 'timestamp' fields, sorting from newest to oldest
-    return strtotime($b['timestamp']) - strtotime($a['timestamp']);
-});
+$repos = sendRequest($ch, "https://api.github.com/orgs/GEWIS/repos?per_page=25&sort=pushed");
 
+//$activities = array();
+//foreach ($repos as $repo) {
+//    echo $repo['name'] . "<br>";
+//    $name = $repo['name'];
+//    $activity = sendRequest($ch, "https://api.github.com/repos/GEWIS/$name/activity");
+//    foreach ($activity as $activityItem) {
+//        $simplifiedActivity = array();
+//        $simplifiedActivity['id'] = $activityItem['id'];
+//        $simplifiedActivity['repo'] = $repo['name'];
+//        $simplifiedActivity['timestamp'] = $activityItem['timestamp'];
+//        $simplifiedActivity['actor'] = $activityItem['actor']['login'];
+//        $simplifiedActivity['gravatar'] = $activityItem['actor']['avatar_url'];
+//        $activities[] = $simplifiedActivity;
+//    }
+//}
+//// Sort activities by timestamp (newest to oldest)
+//usort($activities, function ($a, $b) {
+//    // Compare the 'timestamp' fields, sorting from newest to oldest
+//    return strtotime($b['timestamp']) - strtotime($a['timestamp']);
+//});
+//
 //foreach ($activities as $activity) {
 //    $id = $activity['id'];
 //    $repo = $activity['repo'];
-//    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/GEWIS/$repo/issues/events/$id");
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-//        "Accept: application/vnd.github+json",
-//        "Authorization: Bearer $accessToken",
-//        "X-GitHub-Api-Version: 2022-11-28",
-//        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0"
-//    ]);
-//    $response = curl_exec($ch);
+//    $response = sendRequest($ch, "https://api.github.com/repos/GEWIS/$repo/issues/events/$id");
 //    echo $response;
-//    curl_close($ch);
 //    foreach ($repos as $repo) {
 //        echo $repo['full_name'] . "<br>";
 //    }
 //}
 
-$commit_count = array();
-$since = date('Y-m-d\TH:i:s\Z', strtotime('-30 days'));
+$contributors = array();
+$since = date('Y-m-d\TH:i:s\Z', strtotime('-14 days'));
 foreach ($repos as $repo) {
-    $repo_name = $repo['full_name'];
-    $url = "https://api.github.com/repos/$repo_name/commits?since=$since";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Accept: application/vnd.github+json",
-        "Authorization: Bearer $accessToken",
-        "X-GitHub-Api-Version: 2022-11-28",
-        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0"
-    ]);
-
-    $response = curl_exec($ch);
-    $commits = json_decode($response, true);
-    curl_close($ch);
+    $repo_name = $repo['name'];
+    $commits = sendRequest($ch, "https://api.github.com/repos/GEWIS/$repo_name/commits?since=$since");
 
     foreach ($commits as $commit) {
-        $author = isset($commit['author']['login']) ? $commit['author']['login'] : '';
+        $author = $commit['author']['login'] ?? '';
         if ($author == 'dependabot[bot]') {
             continue;
         }
-        $count = isset($commit_count[$author]) ? $commit_count[$author] : 0;
+        $count = $contributors[$author]['count'] ?? 0;
+        if (!in_array($repo_name, $contributors[$author]['repos'] ?? [])) {
+            $contributors[$author]['repos'][] = $repo_name;
+        }
         $count += 1;
-        $commit_count[$author] = $count;
+        $contributors[$author]['count'] = $count;
+        $contributors[$author]['image'] = $commit['author']['avatar_url'];
     }
 }
 
-arsort($commit_count);
+uasort($contributors, function ($a, $b) {
+    return $b['count'] <=> $a['count'];
+});
 
-//$top_five = array_slice($commit_count, 0, 5);
+curl_close($ch);
+?>
 
-foreach ($commit_count as $author => $commitCount) {
-    echo "Author: $author, Commits: $commitCount<br>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GitHub Commit Report</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+foreach ($contributors as $author => $contributor) {
+    $imageUrl = $contributor['image'];
+    $repoList = implode(', ', $contributor['repos']);
+    echo "
+<div class='author'>
+    <img src='$imageUrl' alt='Avatar of $author' class='avatar'>
+    <h2 class='author-name'><span class='highlight'>$author</span></h2>
+    <div class='info'>
+        <p class='commit-count'><strong>" . $contributor['count'] . "</strong> Contributions</p>
+        <p class='contributed-repos'>Contributed to: </><i>" . $repoList . "</i></p>
+    </div>
+</div>";
 }
 ?>
+
+</body>
+</html>
