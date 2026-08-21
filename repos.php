dd08a5d08f15718bca87804ed93f5a157ca7cfa9
Tom Udding <tomudding@users.noreply.github.com>
2026-08-21 16:47:12 +0200

    chore: update watches repos
    Quite some things have changed since this was last edited, so this reflects that. This also includes some changes made on the web host that have not been brought back here.

--- repos.php
+++ repos.php
@@ -2,14 +2,10 @@
 declare(strict_types=1);
 
 $reposToWatch = [
-    ["owner" => "GEWIS", "repo" => "gewisweb"],
-    ["owner" => "GEWIS", "repo" => "gewisdb"],
-    ["owner" => "GEWIS", "repo" => "sudosos-backend"],
-    ["owner" => "GEWIS", "repo" => "aurora-core"],
-    ["owner" => "GEWIS", "repo" => "sudosos-frontend"],
+    ["owner" => "GEWIS", "repo" => "radix"],
+    ["owner" => "GEWIS", "repo" => "sudosos"],
+    ["owner" => "GEWIS", "repo" => "aurora"],
     ["owner" => "GEWIS", "repo" => "parelpracht"],
-    ["owner" => "GEWIS", "repo" => "aurora-backoffice"],
-    ["owner" => "GEWIS", "repo" => "aurora-client"],
     ["owner" => "GEWIS", "repo" => "grooster-backend"],
     ["owner" => "GEWIS", "repo" => "grooster-frontend"],
 ];
